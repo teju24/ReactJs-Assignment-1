1431fc32793d9b7ea43702fe0146f658e57201fd
teju24 <tejashreenabde242@gmail.com>
2020-04-21 18:09:11 +0530

    updated question 2

--- question2/src/ProjectManagementSystem.js
+++ question2/src/ProjectManagementSystem.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import ProjectLogo from './ProjectLogo';
+import SearchComponent from './SearchComponent'
+import HeaderComponent from './HeaderComponent'
+import ProjectDetailsComponent from './ProjectDetailsComponent'
 import './ProjectManagementSystem.css';
 
 
-class ProjectManagementSystem extends React.Component{
-    render(){
-        return(
+class ProjectManagementSystem extends React.Component {
+    render() {
+        var projects=[
+            {
+                Id: 1,
+                Name: "TCI",
+                scrumMasterName: "Vishal",
+                teamMemberCount:20
+
+            },
+            {
+                Id: 2,
+                Name: "JD",
+                scrumMasterName: "Tejashree",
+                teamMemberCount:10
+            },
+            {
+                Id: 3,
+                Name: "John deere",
+                scrumMasterName: "Monika",
+                teamMemberCount:30
+            },
+            {
+                Id: 4,
+                Name: "Yash",
+                scrumMasterName: "Bhagyashree",
+                teamMemberCount:20
+            }
+        ]
+        return (
+            
             <div>
-              <h2> <ProjectLogo/><span className="title">Project Management System </span></h2>
-            </div>
+                    <div><ProjectLogo /></div>
+                    <div>
+                    <SearchComponent/>
+                    </div>
+                    <div>
+                    <HeaderComponent/>
+                    </div><br></br>
+                    <div>
+                    <table>
+                    {
+                        projects.map((project)=>{
+                           return <ProjectDetailsComponent key={project.Id} project={project}/>
+                        })
+                    }
+                    </table>
+                    </div>
+                    <div>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </div>
+                    
+                    </div>
         )
     }
-
 }
 export default ProjectManagementSystem;
--- question5/src/CustomerInfo.js
+++ question5/src/CustomerInfo.js
@@ -27,11 +27,13 @@ class CustomerInfo extends React.Component {
                 custAddress: "India"
             }
         ]
+        
         var selectedCustomer = customerInfo.filter(function (customer) {
             return ((customer.custName).startsWith('A') && customer.custAddress === "India")
         }).map(function (customer) {
         return (<tbody><tr key={customer.custId} ><td>{customer.custId}</td><td>{customer.custName}</td><td>{customer.custAddress}</td></tr></tbody>)
         })
+
         return (
             <div>
                 <div>
